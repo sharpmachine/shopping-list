09d3f9ea2d55f5fa32e394140104969abf2dd1af
Jesse Kuntz <jesseckuntz@gmail.com>
2017-05-09 09:47:54 -0700

    Add documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,6 @@ import { Component, OnInit } from '@angular/core';
 
 import * as _ from 'lodash';
 
-// TODO: Edit item
-// TODO: Create item
-// TODO: Delete item
-
 // TODO: Strikethrough when list item is checked
 
 @Component({
@@ -25,6 +21,33 @@ export class AppComponent implements OnInit {
     this.shoppingList.items = [];
   }
 
+
+  // Item methods
+
+  getItems() {
+    // TODO: Get items
+  }
+
+  createItem() {
+    // TODO: Create item
+  }
+
+  updateItem() {
+    // TODO: Update item
+  }
+
+  deleteItem() {
+    // TODO: Delete item
+  }
+
+
+  /**
+   * @description Add an item to a list
+   *
+   * @param {Item} item
+   *
+   * @memberof AppComponent
+   */
   addItemToShoppingList(item: Item) {
     const isItemInList = Boolean(_.find(this.shoppingList.items, { 'name': item.name }));
 
@@ -38,24 +61,42 @@ export class AppComponent implements OnInit {
     this.getTotal();
   }
 
-  getTotal() {
-    let total = 0;
-    _.forEach(this.shoppingList.items, item => {
-      total = (item.price * item.quantity) + total;
-    });
 
-    this.total = total;
+  // List methods
+
+  getList() {
+    // TODO: Get List
   }
 
-  saveList() {
+  updateList() {
     // TODO: Save list when item is added, removed or quantity changes
   }
 
+  clearList() {
+    // TODO: Reset list
+  }
+
+
+  /**
+   * @description Increase quantity of a list item
+   *
+   * @param {Item} item
+   *
+   * @memberof AppComponent
+   */
   increaseQty(item: Item) {
     item.quantity = item.quantity + 1;
     this.getTotal();
   }
 
+
+  /**
+   * @description descrease quantity of a list item
+   *
+   * @param {Item} item
+   *
+   * @memberof AppComponent
+   */
   decreaseQty(item: Item) {
     if (item.quantity > 1) {
       item.quantity = item.quantity - 1;
@@ -63,11 +104,36 @@ export class AppComponent implements OnInit {
     }
   }
 
+
+  /**
+   * @description remove an item from the list
+   *
+   * @param {Item} item
+   *
+   * @memberof AppComponent
+   */
   removeFromList(item: Item) {
     item.quantity = 1;
     const index = _.findIndex(this.shoppingList.items, item);
     this.shoppingList.items.splice(index, 1);
     this.getTotal();
+  }
+
+
+  /**
+   * @description get cost totals
+   *
+   * @private
+   *
+   * @memberof AppComponent
+   */
+  private getTotal() {
+    let total = 0;
+    _.forEach(this.shoppingList.items, item => {
+      total = (item.price * item.quantity) + total;
+    });
+
+    this.total = total;
   }
 
 }
