830de3c9f74ddb423c3426ed099e5aa6d23cb68a
Jesse Kuntz <jesseckuntz@gmail.com>
2017-05-10 09:38:03 -0700

    Clean up comments

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const jsonServer = require('json-server');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+
 app.use(express.static(__dirname + '/dist'));
 app.use(jsonServer.router('db/db.json'));
-// Start the app by listening on the default
-// Heroku port
-// app.listen(process.env.PORT || 8080);
 app.listen(process.env.PORT || 3000);
