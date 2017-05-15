49cb67df692ee7b87fd074724650bc6f6e83c965
Jesse Kuntz <jesseckuntz@gmail.com>
2017-05-15 10:16:49 -0700

    Create seperate DBs for specific environments

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const jsonServer = require('json-server');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.use(jsonServer.router('db/db.json'));
+app.use(jsonServer.router('db/prod-db.json'));
 app.listen(process.env.PORT || 3000);
