4f7db35d41dbc198258c219ecee083edcb02ad34
Jesse Kuntz <jesseckuntz@gmail.com>
2017-05-18 12:15:03 -0700

    Add MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
-const express = require('express');
-const jsonServer = require('json-server');
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
+var ObjectID = mongodb.ObjectID;
+require('dotenv').config();
 
-app.use(express.static(__dirname + '/dist'));
-app.use(jsonServer.router('db/prod-db.json'));
-app.listen(process.env.PORT || 3000);
+var CONTACTS_COLLECTION = 'contacts';
+
+var app = express();
+app.use(bodyParser.json());
+
+// mongoose.connect(process.env.MONGODB_URI);
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongoose.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log('Database connection ready');
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log('App now running on port', port);
+  });
+});
+
+// CONTACTS API ROUTES BELOW
